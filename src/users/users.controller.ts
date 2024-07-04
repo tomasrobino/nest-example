19a949d40e064dfaf53a9c3f2a55a50ebe5877da
Tomas <tomaserobino@gmail.com>
2024-07-04 11:49:57 +0200

    services done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    //Get all users
+    @Get() //GET /users or /users?role=value
+    getAll(@Query("role") role?: "INTERN" | "EMPLOYEE" | "ADMIN"): [] {
+        return [];
+    }
+
+    //Get specific user
+    @Get(':id') //GET /users/:id
+    getSingle(@Param(":id") id: string): {id: string} {
+        return { id }
+    }
+
+    //Create new user
+    @Post() //POST /users
+    create(@Body() user: {}): {} {
+        return user;
+    }
+
+    @Patch(":id") //PATCH /users/:id
+    update(@Param("id") id: string, @Body() body: string) {
+        return { id };
+    }
+
+    @Delete(":id") //DELETE /users/:id
+    delete(@Param("id") id: string): {id:string} {
+        return {id}
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,62 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: {id: number, name: string, email: string, role: "EMPLOYEE" | "ADMIN" | "INTERN"}[] = [
+        {
+            id: 1,
+            name: "a b",
+            email: "a@b.com",
+            role: "EMPLOYEE"
+        },
+        {
+            id: 2,
+            name: "c d",
+            email: "c@d.com",
+            role: "ADMIN"
+        },
+        {
+            id: 3,
+            name: "e f",
+            email: "e@f.com",
+            role: "INTERN"
+        },
+        {
+            id: 4,
+            name: "g h",
+            email: "g@h.com",
+            role: "EMPLOYEE"
+        },
+    ]
+
+    getAll(role?: "INTERN" | "EMPLOYEE" | "ADMIN") {
+        if (role) {
+            this.users.filter(user => user.role === role );
+        }
+        return this.users
+    }
+
+    getSingle(id: number) {
+        return this.users.find(user => user.id === id);
+    }
+
+    create(user: { name: string, email: string, role: "EMPLOYEE" | "ADMIN" | "INTERN"}) {
+        this.users.push({id: this.users.slice(-1)[0].id+1, ...user});
+    }
+
+    update(id: number, updatedUser: {name: string, email: string, role: "EMPLOYEE" | "ADMIN" | "INTERN"}) {
+        const gotId: number | undefined = this.users.findIndex(element => element.id === id);
+        if (gotId) {
+            this.users[gotId] = {...this.users[gotId], ...updatedUser};
+            return 0;
+        } else return 1;
+    }
+
+    delete(id: number) {
+        const gotId: number | undefined = this.users.findIndex(element => element.id === id);
+        if (gotId) {
+            this.users.splice(gotId, 1);
+            return 0;
+        } else return 1;
+    }
+}
